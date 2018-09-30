5e3aefaa583822a41fd3a69a8e33717e817cdb6b
BBvLucky <35274237+BBvLucky@users.noreply.github.com>
2018-09-30 20:48:09 +0300

    Задание с кофеваркой можно считать завершенным

--- script.js
+++ script.js
@@ -1,42 +1,54 @@
 function CoffeeMachine(power) {
     // удельная теплоемкость воды
-    const waterHeatCapacity = 4200
+    const waterHeatCapacity = 4200;
     // температура воды до которой греем
-    const maxTemp = document.getElementById('temp').value
+    const maxTemp = document.getElementById('temp').value;
     // колличество воды в кофеварке
-    let waterAmount = document.getElementById('water').value
+    const waterAmount = document.getElementById('water').value;
+    //кол-во кофейных зёрен
+    const beansCount = document.getElementById('beans').value;
 
-    console.log(maxTemp);
+    //информационное табло кофеварки
+    let infoPanel = document.getElementById('info');
 
-    // this.fill = function (newAmount) {
-    //   if(newAmount >= 50) {
-    //     waterAmount = newAmount
-    //   } else {
-    //     console.error('Слишком мало воды!')
-    //   }
-    // }
-
+    //ф-я расчёта времени варки
     const getBoilTime = function () {
-      return (waterAmount * waterHeatCapacity * maxTemp) / power
+      return (waterAmount * waterHeatCapacity * maxTemp * beansCount) / power;
     }
-
+    //ф-я варки
     this.launch = function () {
       const time = getBoilTime()
-      console.info(`Время ожидания: ${time}`)
-      setTimeout(function () {
-        console.log('Ваш кофе готов!')
+      if (getBoilTime() <= 0) {
+        infoPanel.innerText = 'Кофеварка не может быть пустой!';
+      } 
+      else if (maxTemp < 70) {
+        infoPanel.innerText = 'Недостаточная температура!';
+      } else {
+        infoPanel.innerText = `Время ожидания: ${Math.round(time/1000)} сек.`;
+
+      var cookTimer = setTimeout(function () {
+        infoPanel.innerText = 'Ваш кофе готов!';
       }, time)
+      }
+      
+      //кнопка остановки процесса варки
+      const stop = document.getElementById('stop');
+      stop.addEventListener('click', stopCook);
+      
+      //ф-я остановки варки
+      function stopCook() {
+        clearTimeout(cookTimer);
+        infoPanel.innerHTML = 'Операция прервана!';
+      }
     }
   }
 
-//   var vitek = new CoffeeMachine(3000)
-//   vitek.fill(50)
-//   vitek.launch()
-
+  //кнопка зауска
 const start = document.getElementById('start');
 start.addEventListener('click', startCook);
 
+//ф-я запуска
 function startCook() {
-    var vitek = new CoffeeMachine(3000);
-    vitek.launch();
+    var kidtek = new CoffeeMachine(3000);
+    kidtek.launch();
 }
